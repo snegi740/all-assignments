507dac80d37ae79f97403a9ad692521167759e5a
snegi740 <60218212+snegi740@users.noreply.github.com>
2020-02-07 18:06:43 +0530

    Calculator by Java and C#

---
+++ MyCalculator.java
@@ -0,0 +1,68 @@
+package javafourquestions;
+import java.util.Scanner;
+
+public class MyCalculator {
+	public static void main(String args[]) {
+		
+	 System.out.println("Enter 1st number :");
+	 Scanner s1 = new Scanner(System.in);
+		int p1 = s1.nextInt();
+	 System.out.println("Enter 2nd number :");
+	 Scanner s2 = new Scanner(System.in);
+		int p2 = s2.nextInt();
+	 System.out.println("Enter 1 for Addition :");
+	 System.out.println("Enter 2 for Subtraction :");
+	 System.out.println("Enter 3 for Multiplication :");
+	 System.out.println("Enter 4 for Division :");
+	 System.out.println("Enter 5 for Power :");
+
+	 System.out.println("Enter operation :");
+	 Scanner s3 = new Scanner(System.in);
+		int p3 = s3.nextInt();
+	 
+
+
+     callMyMethod(p1, p2, p3);
+     
+	}
+     // Console.WriteLine();
+
+ 
+	public static void callMyMethod( int a, int b, int c) {
+ 
+		switch (c)
+		{
+         	case 1:
+         		System.out.println(a + b);
+         		break;
+         	case 2:
+         		System.out.println(a - b);
+         		break;
+         	case 3:
+         		System.out.println(a * b);
+         		break;
+         	case 4:
+         		System.out.println(a / b);
+         		break;
+         	case 5:
+         		System.out.println(power(a,b));
+         		break;
+         	default:
+         		System.out.println("enter correct option for operation");
+         		break;
+		}
+		}
+		public static int power(int x, int y)
+		{
+			int i;
+			int sum = 1;
+			for (i = 0; i < y; i++)
+			{
+				sum = sum * x;
+
+			}
+			return sum;
+
+	}
+
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter 1st number :");
+            int num1 = Convert.ToInt32( Console.ReadLine());
+            Console.Write("Enter 2nd number :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 1 for Addition :");
+            Console.WriteLine("Enter 2 for Subtraction :");
+            Console.WriteLine("Enter 3 for Multiplication :");
+            Console.WriteLine("Enter 4 for Division :");
+            Console.WriteLine("Enter 5 for Power :");
+
+            int op = Convert.ToInt32(Console.ReadLine());
+
+
+            callMyMethod(num1, num2, op);
+            
+
+            // Console.WriteLine();
+
+        }
+        static void callMyMethod( int a, int b, int c)
+        {
+            switch (c)
+            {
+                case 1:
+                    Console.WriteLine(a + b);
+                    break;
+                case 2:
+                    Console.WriteLine(a - b);
+                    break;
+                case 3:
+                    Console.WriteLine(a * b);
+                    break;
+                case 4:
+                    Console.WriteLine(a / b);
+                    break;
+                case 5:
+                    Console.WriteLine(power(a,b));
+                    break;
+                default:
+                    Console.WriteLine("enter correct option for operation");
+                    break;
+            }
+            double power(int x, int y)
+            {
+                int i;
+                int sum = 1;
+                for (i = 0; i < y; i++)
+                {
+                    sum = sum * x;
+
+                }
+                return sum;
+            }
+
+            
+        }
+    }
+}
