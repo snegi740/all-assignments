e838a95b70d094acd571d77d0badc2919412e14a
snegi740 <60218212+snegi740@users.noreply.github.com>
2020-02-10 14:47:17 +0530

    Queue to perform adding, removing, display

--- ImplementQueue.java
+++ ImplementQueue.java
@@ -6,22 +6,34 @@ public class ImplementQueue {
 		
 		NoobQueue q = new NoobQueue();
 		
-		q.adding(4);
-		q.adding(4);
-		q.adding(4);
-		q.adding(4);
-	
-		
-		q.removing();
-		q.adding(5);
-		
-		q.display();
-		
-		
-		System.out.println("Emptiness of queue is " + q.isEmpty());
-		System.out.println("Size " + q.getSize() );
-		System.out.println("Fullness of queue is " + q.isFull());
-		
+		int ch;
+        do
+        {   System.out.println("\n" + "Enter your choice");
+            System.out.println("1.To add an element");
+            System.out.println("2.To remove first item of queue");
+            System.out.println("3.To display your queue");
+            System.out.println("4.Exit from this operation ");
+            
+            ch= new Scanner(System.in).nextInt();
+   
+            switch(ch)
+                {
+                    case 1: System.out.println("Enter Integer to add to queue");
+                            int value=new Scanner(System.in).nextInt();   
+                            q.adding(value);
+                            break;
+                    case 2: System.out.println("Removing first item of queue");
+                            q.removing();
+                            break;
+                    case 3: 
+                            q.display();
+                            break;
+                    case 4:
+                            break;
+                    default:
+                            System.out.println("Wrong choice");
+                }
+        }while(ch!=4);
 	}
 
 }
